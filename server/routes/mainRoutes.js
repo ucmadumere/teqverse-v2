f020ee7b2d829377a641259e8ba1157fa9a301ea
ucmadumere <ucyweb@gmail.com>
2024-01-17 18:49:24 +0100

    search

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const  router = express.Router();
+const router = express.Router();
 const Postjob = require('../models/postJob');
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
@@ -8,27 +8,9 @@ const verifyToken = require('../../utilities/verifyToken');
 const bcrypt = require('bcrypt')
 
 
-// Middleware to handle pagination
-// app.use((req, res, next) => {
-//     const page = parseInt(req.query.page) || 1; // Get the requested page or default to 1
-//     const itemsPerPage = 10; // Set the number of items per page
-  
-//     const startIndex = (page - 1) * itemsPerPage;
-//     const endIndex = startIndex + itemsPerPage;
-  
-//     res.locals.pagination = {
-//       currentPage: page,
-//       totalPages: Math.ceil(data.length / itemsPerPage),
-//     };
-  
-//     res.locals.data = data.slice(startIndex, endIndex);
-  
-//     next();
-//   });
-
 // Routes
 
-router.get('/', (req,res) => {
+router.get('/', (req, res) => {
     res.render('index');
 });
 
@@ -36,51 +18,51 @@ router.get('/', (req,res) => {
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                               SIGN IN                                              **/
 /**--------------------------------------------------------------------------------------------------- **/
-router.get('/login',  (req, res) => {
-    res.render('login', { user:req.User });
-  });
+router.get('/login', (req, res) => {
+    res.render('login', { user: req.User });
+});
 
-  router.post('/login', async (req, res) => {
+router.post('/login', async (req, res) => {
     try {
-      const { email, password } = req.body;
-  
-      // Check if the user exists
-      const user = await User.findOne({ email });
-      if (!user) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      // Validate the password
-      const isPasswordValid = await bcrypt.compare(password, user.password);
-      if (!isPasswordValid) {
-        return res.status(401).json({ message: 'Invalid email or password' });
-      }
-  
-      // Generate JWT token
-      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
-      console.log(token)
-      
-      res.json('/');
+        const { email, password } = req.body;
+
+        // Check if the user exists
+        const user = await User.findOne({ email });
+        if (!user) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        // Validate the password
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(401).json({ message: 'Invalid email or password' });
+        }
+
+        // Generate JWT token
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        console.log(token)
+
+        res.json('/');
 
     } catch (error) {
-      console.error(error);
-      res.status(500).json({ message: 'Server Error' });
+        console.error(error);
+        res.status(500).json({ message: 'Server Error' });
     }
-  });
-  
-  
+});
+
+
 
 
 
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                               SIGN UP                                              **/
 /**--------------------------------------------------------------------------------------------------- **/
-router.get('/signup', (req,res) => {
+router.get('/signup', (req, res) => {
     res.render('signup');
-}); 
- router.post('/signup', async (req, res) => {
+});
+router.post('/signup', async (req, res) => {
     try {
-        const {name, email, password} = req.body;
+        const { name, email, password } = req.body;
 
         const existingUser = await User.findOne({ email });
         if (existingUser) {
@@ -96,120 +78,64 @@ router.get('/signup', (req,res) => {
         await newUser.save();
 
         res.redirect('/login');
-    }catch (error) {
+    } catch (error) {
         console.error(error);
         res.status(500).send('Server Error');
     }
- });
-
-
-router.get('/blog', (req,res) => {
-    res.render('blog');
 });
 
 
-// router.get('/joblist', async (req, res) => {
-//     try {
-//       const locals = {
-//         title: 'TeqVerse',
-//         description: 'Job List',
-//       };
-  
-//       const page = parseInt(req.query.page) || 1;
-//       const pageSize = 5; // Number of items per page
-  
-//       const totalJobs = await Postjob.countDocuments();
-//       const totalPages = Math.ceil(totalJobs / pageSize);
-  
-//       const jobs = await Postjob.find()
-//         .skip((page - 1) * pageSize)
-//         .limit(pageSize);
-  
-//       res.render('joblist', {
-//         data: jobs,
-//         locals,
-//         page,
-//         totalPages,
-//       });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send('Internal Server Error');
-//     }
-//   });
-
-// router.get('/joblist', async (req, res) => {
-//     try {
-//       const locals = {
-//         title: 'TeqVerse',
-//         description: 'Job List',
-//       };
-  
-//       const page = parseInt(req.query.page) || 1;
-//       const pageSize = 5; // Number of items per page
-  
-//       const totalJobs = await Postjob.countDocuments();
-//       const totalPages = Math.ceil(totalJobs / pageSize);
-  
-//       const jobs = await Postjob.find()
-//         .skip((page - 1) * pageSize)
-//         .limit(pageSize);
-  
-//       res.render('joblist', {
-//         data: jobs,
-//         locals,
-//         page,
-//         totalPages,
-//       });
-//     } catch (error) {
-//       console.error(error);
-//       res.status(500).send('Internal Server Error');
-//     }
-//   });
+router.get('/blog', (req, res) => {
+    res.render('blog');
+});
+/**--------------------------------------------------------------------------------------------------- **/
+/**                                               JOB & DETAILS STARTS HERE                            **/
+/**--------------------------------------------------------------------------------------------------- **/
 router.get('/joblist', async (req, res) => {
     try {
-      const locals = {
-        title: 'TeqVerse',
-        description: 'Job List',
-      };
-  
-      const page = parseInt(req.query.page) || 1;
-      const pageSize = 5; // Number of items per page
-      let query = {};
-  
-      // Check if a search term is provided in the query parameters
-      const searchTerm = req.query.q;
-      if (searchTerm) {
-        // Use a case-insensitive regex to match the search term in title or body
-        query = {
-          $or: [
-            { title: { $regex: searchTerm, $options: 'i' } },
-            { body: { $regex: searchTerm, $options: 'i' } },
-          ],
+        const locals = {
+            title: 'TeqVerse',
+            description: 'Job List',
         };
-      }
-  
-      const totalJobs = await Postjob.countDocuments(query);
-      const totalPages = Math.ceil(totalJobs / pageSize);
-  
-      const jobs = await Postjob.find(query)
-        .skip((page - 1) * pageSize)
-        .limit(pageSize);
-  
-      res.render('joblist', {
-        data: jobs,
-        locals,
-        page,
-        totalPages,
-        searchTerm,
-      });
-    } catch (error) {
-      console.error(error);
-      res.status(500).send('Internal Server Error');
-    }
-  });
 
-router.get('/jobdetails', (req,res) => {
-    
+        const page = parseInt(req.query.page) || 1;
+        const pageSize = 5; // Number of items per page
+        let query = {};
+
+        // Check if a search term is provided in the query parameters
+        const searchTerm = req.query.q;
+        if (searchTerm) {
+            // Use a case-insensitive regex to match the search term in title or body
+            query = {
+                $or: [
+                    { title: { $regex: searchTerm, $options: 'i' } },
+                    { body: { $regex: searchTerm, $options: 'i' } },
+                ],
+            };
+        }
+
+        const totalJobs = await Postjob.countDocuments(query);
+        const totalPages = Math.ceil(totalJobs / pageSize);
+
+        const jobs = await Postjob.find(query)
+            .skip((page - 1) * pageSize)
+            .limit(pageSize);
+
+        res.render('joblist', {
+            data: jobs,
+            locals,
+            page,
+            totalPages,
+            searchTerm,
+        });
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Internal Server Error');
+    }
+});
+
+router.get('/jobdetails', (req, res) => {
+
     res.render('jobdetails');
 });
 
@@ -222,21 +148,25 @@ router.get('/jobdetails/:id', async (req, res) => {
         const locals = {
             title: "TeqVerse",
             description: "Job Detail"
-          }
-      let slug = req.params.id;
-  
-      const data = await Postjob.findById({ _id: slug });
-  
-      res.render('jobdetails', { 
-        locals,
-        data,
-        currentRoute: `/jobdetails/${slug}`
-      });
+        }
+        let slug = req.params.id;
+
+        const data = await Postjob.findById({ _id: slug });
+
+        res.render('jobdetails', {
+            locals,
+            data,
+            currentRoute: `/jobdetails/${slug}`
+        });
     } catch (error) {
-      console.log(error);
+        console.log(error);
     }
-  
-  });
+
+});
+
+/**--------------------------------------------------------------------------------------------------- **/
+/**                                               JOB & DETAILS ENDS HERE                                **/
+/**--------------------------------------------------------------------------------------------------- **/
 
 
 module.exports = router;
