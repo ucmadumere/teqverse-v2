dde0c8ef285b668c53a37223b7192b06b57a2fc8
Ikechukwu <ikechukwuandrew4@gmail.com>
2024-01-17 17:16:07 +0100

    login-page done

--- utilities/verifyToken.js
+++ utilities/verifyToken.js
@@ -3,12 +3,13 @@ const jwt = require('jsonwebtoken');
 const verifyToken = (req, res, next) => {
     const token = req.headers.authorization;
     if (!token) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return res.status(401).json({ message: 'Unauthorized: Missing token' });
     }
 
-    jwt.verify(token, 'your_secret_key', (err, decoded) => {
+    jwt.verify(token.split(' ')[1], process.env.JWT_SECRET, (err, decoded) => {
         if (err) {
-            return res.status(401).json({ message: 'Invalid token' });
+            console.error('Token verification error:', err);
+            return res.status(401).json({ message: 'Unauthorized: Invalid token' });
         }
         req.userId = decoded.userId;
         next();
