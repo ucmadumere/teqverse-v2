8b1c811aee275cc48e19fb45ec2d1757e008dd85
ucmadumere <ucyweb@gmail.com>
2024-01-21 20:54:59 +0100

    login

--- server/routes/adminRoutes.js
+++ server/routes/adminRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const adminauthController = require('../controllers/adminauthController')
+const adminLayout = '../views/layouts/adminLogin';
 
 
 
@@ -17,9 +18,31 @@ router.post('/create-superuser', adminauthController.createSuperuser);
 /**                                   LOGIN ROUTE                                                      **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/login-superuser', (req, res) => {
-  res.render('admin/signin');
+  // res.render('admin/signin');
+  res.render('admin/index', {layout: adminLayout });
 });
 router.post('/login-superuser', adminauthController.loginAdmin);
+
+/**--------------------------------------------------------------------------------------------------- **/
+/**                                   DASHBOARD                                                        **/
+/**--------------------------------------------------------------------------------------------------- **/
+// const getDashboard = async (req, res) => {
+//     try {
+//         const locals = {
+//             title: 'Dashboard'
+//         }
+
+//         const data = await postJob.find();
+//         res.render('admin/dashboard', {
+//             locals,
+//             data,
+//             layout: adminLayout
+//         });
+
+//     } catch (error) {
+//         console.log(error);
+//     }
+// }
 
 
 module.exports = router
