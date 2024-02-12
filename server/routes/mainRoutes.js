7cc0d96548de4789581a1c7aaf7f4a1467f00092
Ikechukwu <ikechukwuandrew4@gmail.com>
2024-02-12 13:13:40 +0100

    reset password functionality

--- server/routes/mainRoutes.js
+++ server/routes/mainRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/userModel');
-const { login, register, logout } = require('../controllers/authController');
+const { login, register, logout} = require('../controllers/authController');
 const jobdetailController = require('../controllers/jobdetailController');
 const joblistController = require('../controllers/joblistController');
 const userLayout = '../views/layouts/userLogin';
@@ -11,19 +11,21 @@ const { requireAuth, checkUser, redirectIfAuthenticated, checkPremiumUser } = re
 
 const profileImageController = require('../controllers/updateProfileController');
 
-const { applyPremiumjob, getApplypremiumJob } = require('../controllers/premiumJobController');
+const {applyPremiumjob, getApplypremiumJob} = require('../controllers/premiumJobController');
 const jobdetail = require('../controllers/jobdetailController');
 
 const Review = require('../models/review');
-const { getUserReview, postUserReview } = require('../controllers/reviewController')
+const {getUserReview, postUserReview} = require('../controllers/reviewController')
 const upload = require('../multerConfig')
 const update = require('../controllers/updateProfileController')
 const updateUser = require('../controllers/userController')
+
 const crypto = require('crypto');
 const nodemailer = require('nodemailer');
 
 
-router.post('/profileimage', checkUser, requireAuth,)
+router.post('/profileimage', checkUser, requireAuth, )
+
 
 
 
@@ -57,41 +59,41 @@ router.get('/', checkUser, async (req, res) => {
 /**                                  FAQ ROUTE                                                         **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/faq', checkUser, (req, res) => {
-  res.render('faq');
+    res.render('faq');
 });
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                  ABOUT US ROUTE                                                    **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/about-us', checkUser, (req, res) => {
-  res.render('about-us');
+    res.render('about-us');
 });
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                  MEDIA ROUTE                                                       **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/media', checkUser, (req, res) => {
-  res.render('media');
+    res.render('media');
 });
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                  RESOURCES ROUTE                                                   **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/resources', checkUser, requireAuth, (req, res) => {
-  res.render('resources');
+    res.render('resources');
 });
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                  LEARNING ROUTE                                                    **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/learning', checkUser, requireAuth, (req, res) => {
-  res.render('learning');
+    res.render('learning');
 });
 router.get('/learning-mentor', (req, res) => {
-  res.render('learning-mentor');
+    res.render('learning-mentor');
 });
 
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                   LOGIN ROUTE                                                      **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/login', checkUser, redirectIfAuthenticated, (req, res) => {
-  res.render('login', { layout: userLayout });
+  res.render('login', {layout: userLayout });
 });
 router.post('/login', login);
 
@@ -99,7 +101,7 @@ router.post('/login', login);
 /**                                  REGISTER ROUTE                                                    **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/signup', checkUser, redirectIfAuthenticated, (req, res) => {
-  res.render('signup', { layout: userLayout });
+  res.render('signup', {layout: userLayout });
 });
 router.post('/signup', register);
 
@@ -111,7 +113,7 @@ router.get('/logout', logout)
 /**                                  FORGOT PASSWORD ROUTE                                                     **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/forgot-password', checkUser, redirectIfAuthenticated, (req, res) => {
-  res.render('forgot-password', { layout: userLayout });
+  res.render('forgot-password', {layout: userLayout });
 });
 
 // Function to generate a random token
@@ -123,6 +125,7 @@ const transporter = nodemailer.createTransport({
   service: 'gmail',
   auth: {
     user: 'chibseze933@gmail.com',
+    // pass: 'chinkoeze@IG',
     pass: 'fmyb bqkv madm hmpx',
   },
 });
@@ -161,12 +164,10 @@ router.post('/forgot-password', async (req, res) => {
     user.resetTokenExpires = Date.now() + 3600000; // Token expires in 1 hour
     await user.save();
 
-
     // Send an email with a link containing the reset token
-    const resetLink = `http://localhost:3000/reset-password/${resetToken}`;
+    const resetLink = `http://localhost:3000/reset-password?token=${resetToken}`;
     const emailSubject = 'Password Reset Request';
     const emailHTML = `<p>You have requested a password reset. Click the following link to reset your password:</p><p><a href="${resetLink}">${resetLink}</a></p>`;
-
 
     await sendEmail(user.email, emailSubject, emailHTML);
 
@@ -186,33 +187,24 @@ router.post('/forgot-password', async (req, res) => {
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                  RESET PASSWORD ROUTE                                                     **/
 /**--------------------------------------------------------------------------------------------------- **/
-router.get('/reset-password/:token', checkUser, redirectIfAuthenticated, async (req, res) => {
+router.get('/reset-password', async (req, res) => {
+  const { token } = req.query;
+
   try {
-
-    const locals = {
-      title: 'TeqVerse - Reset Password'
-  };
-    const token = req.params.token;
-
     // Find the user with the provided reset token
-    const user = await User.findOne({
-      resetToken: token,
-      resetTokenExpires: { $gt: Date.now() }, // Check if the token is still valid
-    });
+    const user = await User.findOne({ resetToken: token, resetTokenExpires: { $gt: Date.now() } });
 
     if (!user) {
       // Token is invalid or expired
       return res.render('reset-password', {
         layout: userLayout,
-        locals,
-        errorMessage: 'Invalid or expired reset token. Please request a new one.',
+        errorMessage: 'Invalid or expired token. Please request a new password reset.',
       });
     }
 
-    // Render the reset password page with the token (hidden input field in the form)
     res.render('reset-password', {
       layout: userLayout,
-      token,
+      token: token, // Pass the token to the reset password form
     });
   } catch (error) {
     console.error(error);
@@ -224,35 +216,31 @@ router.get('/reset-password/:token', checkUser, redirectIfAuthenticated, async (
 });
 
 
-router.post('/reset-password/:token', async (req, res) => {
-  try {
-    const token = req.params.token;
-    const newPassword = req.body.newPassword; // Assuming there's an input field with the name "newPassword"
+router.post('/reset-password', async (req, res) => {
+  const { token, password } = req.body;
 
+  try {
     // Find the user with the provided reset token
-    const user = await User.findOne({
-      resetToken: token,
-      resetTokenExpires: { $gt: Date.now() }, // Check if the token is still valid
-    });
+    const user = await User.findOne({ resetToken: token, resetTokenExpires: { $gt: Date.now() } });
 
     if (!user) {
       // Token is invalid or expired
       return res.render('reset-password', {
         layout: userLayout,
-        errorMessage: 'Invalid or expired reset token. Please request a new one.',
+        errorMessage: 'Invalid or expired token. Please request a new password reset.',
       });
     }
 
     // Update the user's password
-    user.password = newPassword;
-    // Clear the reset token and its expiration time
+    user.password = password;
     user.resetToken = undefined;
     user.resetTokenExpires = undefined;
-    console.log(newPassword)
     await user.save();
 
-   // Redirect to the login page or any other page you prefer
-   res.redirect('/login');
+    res.render('reset-password', {
+      layout: userLayout,
+      successMessage: 'Password reset successfully. You can now login with your new password.',
+    });
   } catch (error) {
     console.error(error);
     res.render('reset-password', {
@@ -261,7 +249,6 @@ router.post('/reset-password/:token', async (req, res) => {
     });
   }
 });
-
 
 
 /**--------------------------------------------------------------------------------------------------- **/
@@ -278,10 +265,10 @@ router.get('/joblist', checkUser, requireAuth, joblistController);
 /**                                  JOB FILTER ROUTE                                                  **/
 /**--------------------------------------------------------------------------------------------------- **/
 router.get('/resetfilters', checkUser, requireAuth, (req, res) => {
-  // Redirect to the joblist route without any filter parameters
-  res.redirect('/joblist');
+    // Redirect to the joblist route without any filter parameters
+    res.redirect('/joblist');
 });
-
+          
 /**--------------------------------------------------------------------------------------------------- **/
 /**                                  EDIT PROFILE ROUTE                                                **/
 /**--------------------------------------------------------------------------------------------------- **/
@@ -289,14 +276,14 @@ router.get('/update-profile', checkUser, requireAuth, async (req, res) => {
   try {
     const locals = {
       title: 'TeqVerse - Edit Profile'
-  };
+    };
     res.render('edit-profile', {layout: adminLayout, locals});
   } catch (error) {
-    console.error( error);
-    res.status(500).send( error.message);
+    console.error(error);
+    res.status(500).send(error.message);
   }
-  
 });
+
 
 
 router.post('/update-profile', checkUser, requireAuth, updateUser, update, (req, res) => {});
@@ -306,12 +293,12 @@ router.get('/user-profile', checkUser, requireAuth, (req, res) => {
     const locals = {
       title: 'TeqVerse - View Profile'
     };
-    res.render('user-profile', { layout: adminLayout, locals });
+    res.render('user-profile', {layout: adminLayout, locals});
   } catch (error) {
-    console.error(error);
-    res.status(500).send(error.message);
+    console.error( error);
+    res.status(500).send( error.message);
   }
-
+  
 });
 
 
@@ -321,7 +308,7 @@ router.get('/user-profile', checkUser, requireAuth, (req, res) => {
 router.get('/user-review', checkUser, requireAuth, getUserReview);
 
 // POST route to handle adding a review
-router.post('/user-review', checkUser, requireAuth, postUserReview);
+router.post('/user-review',checkUser, requireAuth, postUserReview);
 
 
 
@@ -333,9 +320,6 @@ router.get('/apply-job/:id', requireAuth, checkUser, checkPremiumUser, getApplyp
 
 // Submit job application route
 router.post('/apply-job/:id', checkPremiumUser, requireAuth, checkUser, upload.single('cv'), applyPremiumjob);
-
-
-
 
 
 
